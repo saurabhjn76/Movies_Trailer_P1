ddb79e52cabe94435daf46a2e12f9942b7d883b3
saurabhjn76 <saurabhjn76@gmail.com>
2017-09-11 14:09:00 +0530

    formatted PEP-8 Guidelines

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,6 +5,15 @@ A movies storage list
 '''
 movies = list()
 
-movies.append(Movie("Interstellar", "http://www.interstellarmovie.net/images/downloads/wps/1920/int_wps_1920_farm.jpg", "https://www.youtube.com/watch?v=zSWdZVtXT7E"))
-movies.append(Movie("Inception", "https://img.yescdn.ru/2016/07/20/cover/1b5c10d82b892ba0f2c2e9b42d5df5fe-inception-1469000805.jpeg", "https://www.youtube.com/watch?v=8hP9D6kZseM"))
-movies.append(Movie("The Dark Knight", "http://media1.santabanta.com/full1/Hollywood%20Movies/The%20Dark%20Knight/the-dark-knight-24d.jpg", "https://www.youtube.com/watch?v=yQ5U8suTUw0"))
+movies.append(Movie("Interstellar",
+                    "http://www.interstellarmovie.net/images/downloads/" +
+                    "wps/1920/int_wps_1920_farm.jpg",
+                    "https://www.youtube.com/watch?v=zSWdZVtXT7E"))
+movies.append(Movie("Inception",
+                    "https://img.yescdn.ru/2016/07/20/cover/1b5c10d82b89" +
+                    "2ba0f2c2e9b42d5df5fe-inception-1469000805.jpeg",
+                    "https://www.youtube.com/watch?v=8hP9D6kZseM"))
+movies.append(Movie("The Dark Knight",
+                    "http://media1.santabanta.com/full1/Hollywood%20Movies/" +
+                    "The%20Dark%20Knight/the-dark-knight-24d.jpg",
+                    "https://www.youtube.com/watch?v=yQ5U8suTUw0"))
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from entertainment_center import movies
 from fresh_tomatoes import open_movies_page
 
 '''
-Main class 
+Main class
 '''
 
 if __name__ == '__main__':
-	open_movies_page(movies)
+    open_movies_page(movies)
